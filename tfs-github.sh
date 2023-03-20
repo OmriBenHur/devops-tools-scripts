3824ad433efe2f9a4c1d24f363c26b1ca065c5d4
OmriBenHur <110596448+OmriBenHur@users.noreply.github.com>
2023-03-20 17:29:35 +0200

    Update tfs-github.sh

--- tfs-github.sh
+++ tfs-github.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
-git init --initial-branch=master
+git init 
 git remote add $1 $2
-git lfs track "*.zip"
+default_branch=$(git remote show $1 | awk '/HEAD branch/ {print $NF}')
+git lfs track "*.zip" "*.lib" "*.pdb"
 git fetch --all
 git pull --all
 git branch -r | grep -v '\->' | while read remote; do sb=$(echo $remote | sed 's/.*\///g') ; git branch --track "$sb" "${remote#origin/}"; done
 git remote add ${1}-github $3
-git push ${1}-github master
+git push ${1}-github $default_branch
 git push --all ${1}-github
 git push --tags ${1}-github
